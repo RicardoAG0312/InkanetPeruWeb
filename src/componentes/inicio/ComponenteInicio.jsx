fbe6525fc6e4bcc921fb1d760b9b52f984e68429
RicardoAG0312 <ricardo41216383@gmail.com>
2025-02-14 13:05:39 -0500

    ricardo

--- src/componentes/inicio/ComponenteInicio.jsx
+++ src/componentes/inicio/ComponenteInicio.jsx
@@ -7,7 +7,7 @@ import VideoPortada1 from "../../images/Portada 1.mp4"
 import VideoPortada2 from "../../images/Portada 2.mp4"
 import VideoPortada3 from "../../images/Portada 3.mp4"
 import VideoPortada4 from "../../images/Portada 5.mp4"
-
+import BotonAnimado from "./ui/BotonAnimado";
 
 export default function ComponenteInicio() {
 
@@ -43,7 +43,7 @@ export default function ComponenteInicio() {
                     <h1 style={{fontSize: "80px", fontWeight: "700", color: "white"}}> Nuestros <span style={{fontWeight: "100"}}> Servicios </span> </h1>
                     <p style={{color: "white"}}> <i style={{color: "#1FB3AE"}} className="bi bi-play-fill"></i> Impulsamos la innovación tecnológica a través de nuestro portafolio de soluciones para mejorar la productividad y competitividad de las empresas. </p>
                 </div>
-                <div style={{ display: 'flex', flexDirection: 'column', height: '550px', marginTop: "40px"}}>
+                <div style={{ display: 'flex', flexDirection: 'column', height: '550px', marginTop: "40px", rowGap: "20px"}}>
                     <div style={{ display: 'flex', flex: 1 }}>
                         <div
                         style={{
@@ -56,7 +56,7 @@ export default function ComponenteInicio() {
                         }}
                         >
                         <Image src={require("../../images/Nuestros servicios 1.png")} roundedCircle style={{ width: '244px', height: '245px' }} />
-                        <bold style={{color: "white"}}> <p style={{fontWeight: "700", width: "120px", textAlign: "center"}}> Conectividad en empresas </p> </bold>
+                        <bold style={{color: "white"}}> <p style={{fontWeight: "700", width: "120px", textAlign: "center", fontFamily: "Red Hat Display"}}> Conectividad en empresas </p> </bold>
                         </div>
                         <div
                         style={{
@@ -69,7 +69,7 @@ export default function ComponenteInicio() {
                         }}
                         >
                         <Image src={require("../../images/Nuestros servicios 2.png")} roundedCircle style={{ width: '244px', height: '245px' }} />
-                        <strong style={{color: "white"}}> <p style={{fontWeight: "700", width: "120px", textAlign: "center"}}> Servicios Gestionados </p> </strong>
+                        <strong style={{color: "white"}}> <p style={{fontWeight: "700", width: "120px", textAlign: "center", fontFamily: "Red Hat Display"}}> Servicios Gestionados </p> </strong>
                         </div>
                         <div
                         style={{
@@ -82,7 +82,7 @@ export default function ComponenteInicio() {
                         }}
                         >
                         <Image src={require("../../images/Nuestros servicios 3.png")} roundedCircle style={{ width: '244px', height: '245px' }} />
-                        <strong style={{color: "white"}}> <p style={{fontWeight: "700", width: "200px", textAlign: "center"}}> Telefonia Corporativa y comunicaciones unificadas </p>  </strong>
+                        <strong style={{color: "white"}}> <p style={{fontWeight: "700", width: "200px", textAlign: "center", fontFamily: "Red Hat Display"}}> Telefonia Corporativa y comunicaciones unificadas </p>  </strong>
                         </div>
                     </div>
                     <div style={{ display: 'flex', flex: 1 }}>
@@ -98,7 +98,7 @@ export default function ComponenteInicio() {
                         }}
                         >
                         <Image src={require("../../images/Nuestros servicios 4.png")} roundedCircle style={{ width: '244px', height: '245px' }} />
-                        <strong style={{color: "white", paddingRight: "12px"}}> Ciberseguridad en empresas </strong>
+                        <strong style={{color: "white", paddingRight: "12px", fontFamily: "Red Hat Display"}}> Ciberseguridad en empresas </strong>
                         </div>
                         <div
                         style={{
@@ -111,22 +111,67 @@ export default function ComponenteInicio() {
                         }}
                         >
                         <Image src={require("../../images/Nuestros servicios 5.png")} roundedCircle style={{ width: '244px', height: '245px' }} />
-                        <strong style={{color: "white", paddingLeft: "65px"}} > Servicios Cloud </strong>
+                        <strong style={{color: "white", paddingLeft: "65px", fontFamily: "Red Hat Display"}} > Servicios Cloud </strong>
                         </div>
                     </div>
                 </div>
             </section>
-            <section style={{backgroundColor: "#FAFAFA", height: "600px"}}>
+            <section style={{backgroundColor: "#FAFAFA", height: "auto", padding: "60px 0"}}>
                 <div className="row p-0 m-0 h-100">
                     <div className="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center" >
                         <Image style={{width: "500px", height: "500px"}} src={require("../../images/Diseño sin título(1).png")} rounded />
                     </div>
                     <div className="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-start">
-                        <h1 style={{fontWeight: "100", width: "350px"}}> Contamos con Nuestro Propio <h1 style={{fontWeight: "bolder", width: "180px"}}> Centro de Atención </h1> </h1>
+                        <h1 style={{fontWeight: "100", width: "350px"}}> Contamos con Nuestro Propio <h1 style={{fontWeight: "bold", width: "200px"}}> Centro de Atención </h1> </h1>
                         <p style={{color: "black"}}> <i style={{color: "#1FB3AE"}} className="bi bi-play-fill"></i> Cada vez que un cliente llama frente a una incidencia es atendido por un ingeniero certificado en networking y seguridad. </p>
+                        
+                        <BotonAnimado 
+                            colorIconoBase = "#05AFA5"
+                            colorLetraBase= "black"
+                            colorBordeBase = "black"
+                            colorIconoPress = "white"
+                            fondoTextoPress = "black"
+                            colorLetraPress = "white"
+                        />
                     </div>
                 </div>
             </section>
+            <section style={{backgroundColor: "#3A4040", height: "auto",padding: "48px 150px",}}className="container-fluid">
+                <div>
+                    <h1 style={{ fontSize: "80px", fontWeight: "100", color: "white" }}>
+                    Nuestros <span style={{ fontWeight: "700" }}> Partners </span>
+                    </h1>
+                    <p style={{ color: "white" }}>
+                    <i style={{ color: "#1FB3AE" }} className="bi bi-play-fill"></i> Contamos
+                    con alianzas estratégicas con los principales fabricantes de tecnología,
+                    líderes en la transformación digital.
+                    </p>
+                </div>
+                <div className="container contenedorPartners">
+                    <div className="grid-container">
+                    {[
+                        require("../../images/8.png"),
+                        require("../../images/12.png"),
+                        require("../../images/24.png"),
+                        require("../../images/10.png"),
+                        require("../../images/Fortinet.png"),
+                        require("../../images/26.png"),
+                        require("../../images/18.png"),
+                        require("../../images/16.png"),
+                        require("../../images/20.png"),
+                        require("../../images/22.png"),
+                        require("../../images/28.png"),
+                        require("../../images/30.png"),
+                        require("../../images/32.png"),
+                    ].map((src, index) => (
+                        <div key={index} className="grid-item">
+                        <img src={src} alt={`Imagen ${index + 1}`} className="float-fluid" />
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            </section>
+
         </>
     )
 }
