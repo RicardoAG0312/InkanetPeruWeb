6b9ab22497e298841df369c4cac05b6a5efe3772
RicardoAG0312 <ricardo41216383@gmail.com>
2025-02-21 13:34:28 -0500

    Ricardo

--- src/componentes/conocenos/cobertura/ComponenteCobertura.jsx
+++ src/componentes/conocenos/cobertura/ComponenteCobertura.jsx
@@ -13,19 +13,19 @@ function ComponenteCobertura() {
             <section className='container-fluid p-0 cobertura-section'>
                 <ComponenteBarraNavegacion />
                 <HeaderForm
-                    img = ""
                     titulo = "Cobertura de fibra optica en todo el Perú"
                 />
             </section>
             <section className='container'>
-                <div className='row mt-5 mb-5'>
-                    <div className="col">
-                        <h1>Red en <br></br> el Peru</h1>
-                        <p>Nuestra red de alta velocidad se fundamenta en un anillo de fibra óptica que abarca todo el territorio peruano, garantizando una conexión robusta y eficiente.</p>
-                        <p>En Lima y Callao, la red metropolitana está compuesta por 12 nodos estratégicamente distribuidos, formando una infraestructura central que opera con tecnología Ten Gigabit Ethernet. Su diseño redundante asegura la continuidad del servicio incluso ante posibles fallos en las conexiones principales.</p>
-                        <p>Utilizamos un protocolo de transmisión compatible con redes locales Ethernet, lo que minimiza los retrasos habituales en el mercado.</p>
+                <div className='row m-5'>
+                    <div className="col-md-6 col-12 d-flex justify-content-center align-content-center flex-column">
+                        <h1 style={{fontSize: "4rem", fontWeight: "bold", width: "50%", marginBottom: "15px"}}>Red en <span style={{fontWeight: "100"}}> el Peru </span></h1>
+                        <p style={{color: "black"}}> <i style={{color: "#1FB3AE"}} className="bi bi-play-fill"></i> Nuestra red de alta velocidad se fundamenta en un anillo de fibra óptica que abarca todo el territorio peruano, garantizando una conexión robusta y eficiente. </p>
+                        <p style={{color: "black"}}> <i style={{color: "#1FB3AE"}} className="bi bi-play-fill"></i>  En Lima y Callao, la red metropolitana está compuesta por 12 nodos estratégicamente distribuidos, formando una infraestructura central que opera con tecnología Ten Gigabit Ethernet. Su diseño redundante asegura la continuidad del servicio incluso ante posibles fallos en las conexiones principales.</p>
+                        <p style={{color: "black"}}> <i style={{color: "#1FB3AE"}} className="bi bi-play-fill"></i>  Utilizamos un protocolo de transmisión compatible con redes locales Ethernet, lo que minimiza los retrasos habituales en el mercado. </p>
+
                     </div>
-                    <div className="col display-flex">
+                    <div className="col-md-6 col-12 d-flex justify-content-center align-items-center img-fluid">
                         <img src={MapaPeru} alt='mapaperu' className='mapaperu'/>
                     </div>
                 </div>
@@ -36,14 +36,14 @@ function ComponenteCobertura() {
                         <div className='imagenVineta d-flex justify-content-center'>
                             <img src={CuatroTres} alt='43' className=''/>
                         </div>
-                        <h1 className='mt-5'>Miembro de <span style={{color:"#09AEAF"}}>NAPerú</span></h1>
+                        <h1 className='mt-4'>Miembro de <span style={{color:"#09AEAF"}}>NAPerú</span></h1>
                         <p>Desde 2006, Inkanet forma parte del NAP y, desde 2012, es miembro del Consejo Directivo de la Asociación NAP Perú, que reúne a las principales empresas de telecomunicaciones del país, facilitando el intercambio de datos entre sus redes.</p>
                     </div>
                     <div className="col figuraGeo text-center" style={{maxWidth: "400px"}}>
                         <div className='imagenVineta d-flex justify-content-center'>
                             <img src={CuatroCuatro} alt='43' className=''/>
                         </div>
-                        <h1 className='mt-5'>Velocidad <span style={{color:"#09AEAF"}}>de conexión</span></h1>
+                        <h1 className='mt-4'>Velocidad <span style={{color:"#09AEAF"}}>de conexión</span></h1>
                         <p>Nos ajustamos a los requerimientos de conectividad de tu empresa, ofreciendo un servicio de Internet por fibra óptica que es altamente escalable, confiable y compatible con diversas soluciones de seguridad informática.</p>
                     </div>
                 </div>
